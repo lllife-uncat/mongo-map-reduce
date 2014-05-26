cf14ecfd4dd7e583f3e2ddf0916fbbb55b4f3bc8
Somnuk Wongkhan <somnuk.wk@gmail.com>
2014-05-26 11:40:07 +0700

    test case and number of images

--- test/mrSpec.js
+++ test/mrSpec.js
@@ -31,12 +31,33 @@ describe("[MTLA DB]", function(){
     });
   });
 
-  it("Should map & reduce", function(done){
+  it("Should map case and number of images success", function(done){
+    this.timeout(50000);
+    function map() {
+      emit(this.CaseID, 1);
+    }
+    function reduce(key, values) {
+       return Array.sum(values);
+    }
 
-    this.timeout(5000);
+    var qimage = getCollection("QImageInfo");
+    var options = {
+      out: "_mr_images_count"
+    };
 
+    qimage.mapReduce(map, reduce, options, function(err, docs){
+      console.log(">> Callback <<");
+      console.log(docs);
+    });
+  });
+
+  it.skip("Should map & reduce", function(done){
+
+    this.timeout(5000000);
     function map () {
-      emit(this.AdminUser , this.NumberOfImages);
+      if(!this.IsDelete) {
+        emit(this.AdminUser , this.NumberOfImages);
+      }
     }
 
     function reduce(key, values) {
